e1849b2ff47991fc28bd14d88bed770837e9be89
gw8471 <gw8471@att.com>
2015-03-12 13:21:03 -0400

    Committed by Udacity

---
+++ Final Pitch Perfect /Pitch Perfect/Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,14 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Gary Wang on 3/6/15.
+//  Copyright (c) 2015 self. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
